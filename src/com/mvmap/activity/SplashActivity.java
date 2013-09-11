9ae34e210bec027c832eedf43aaf8afacde9bf9c
Kenneth <kenneth1982@gmail.com>
2013-09-11 19:42:49 +0800

    add splash

--- src/com/mvmap/activity/SplashActivity.java
+++ src/com/mvmap/activity/SplashActivity.java
@@ -5,15 +5,29 @@ import com.mvmap.news.R.layout;
 import com.mvmap.news.R.menu;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.app.Activity;
+import android.content.Intent;
 import android.view.Menu;
 
 public class SplashActivity extends Activity {
 
+	private final int SPLASH_DISPLAY_LENGHT = 3000; //延迟三秒 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash);
+		new Handler().postDelayed(new Runnable(){
+				 
+	        @Override
+	        public void run() {
+	             Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+	             SplashActivity.this.startActivity(intent);
+	             SplashActivity.this.finish();
+	        }
+             
+        }, SPLASH_DISPLAY_LENGHT); 
 	}
 
 	@Override
